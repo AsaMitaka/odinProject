ca6c7cd34b2b333e1c9182d9d7504831e62db9f9
AsaMitaka <cepelits@gmail.com>
2023-04-16 16:34:34 +0300

    project weather v.2

--- weather/script.js
+++ weather/script.js
@@ -1,11 +1,12 @@
-const inputSearch = document.querySelector('.main__search');
-const weatherTitle = document.querySelector('.main__title');
-const weatherTitleCity = document.querySelector('.main__title-city');
-const weatherCloud = document.querySelector('.main__weather-cloud');
-const weatherHumidity = document.querySelector('.main__weather-humidity');
-const weatherTempC = document.querySelector('.main__weather-tempC');
-const weatherVisKm = document.querySelector('.main__weather-visKm');
-const weatherImgBlock = document.querySelector('.main__weather-img');
+const inputSearchElement = document.querySelector('.main__search');
+const weatherTitleElement = document.querySelector('.main__title');
+const weatherTitleCityElement = document.querySelector('.main__title-city');
+const weatherCloudElement = document.querySelector('.main__weather-cloud');
+const weatherHumidityElement = document.querySelector('.main__weather-humidity');
+const weatherTempCElement = document.querySelector('.main__weather-tempC');
+const weatherVisKmElement = document.querySelector('.main__weather-visKm');
+const weatherImgBlockElement = document.querySelector('.main__weather-img');
+const weatherDateElement = document.querySelector('.main__weather-localtime');
 
 const __url = 'http://api.weatherapi.com/v1';
 const __apikey = 'a02bc0268d5e4593bf4171253231304';
@@ -15,7 +16,7 @@ const fetchData = async (q = 'London') => {
     const data = await fetch(`${__url}/current.json?key=${__apikey}&q=${q}`);
     const resjson = await data.json();
     const weatherData = {
-      localtime: resjson.location.localtime,
+      localtime: resjson.location.localtime.replaceAll('-', ' '),
       country: resjson.location.country,
       region: resjson.location.region,
       city: resjson.location.name,
@@ -25,8 +26,8 @@ const fetchData = async (q = 'London') => {
       vis_km: resjson.current.vis_km,
       cloud: resjson.current.cloud,
 
-      imageSrc: resjson.condition?.icon,
-      imageAlt: resjson.condition?.text,
+      imageSrc: resjson.current.condition?.icon,
+      imageAlt: resjson.current.condition?.text,
     };
 
     return weatherData;
@@ -35,41 +36,52 @@ const fetchData = async (q = 'London') => {
   }
 };
 
+const resetWeather = () => {
+  weatherTitleElement.textContent = 'Error';
+  weatherTitleCityElement.textContent = '';
+  weatherCloudElement.textContent = '';
+  weatherHumidityElement.textContent = '';
+  weatherTempCElement.textContent = '';
+  weatherVisKmElement.textContent = '';
+  weatherDateElement.textContent = '';
+  weatherImgBlockElement.innerHTML = '';
+};
+
 const renderWeather = (data) => {
   if (!data) {
-    titleElement.textContent = 'Error';
-    cityElement.textContent = '';
-    cloudElement.textContent = '';
-    humidityElement.textContent = '';
-    tempElement.textContent = '';
-    visElement.textContent = '';
-    imageElement.innerHTML = '';
+    resetWeather();
     return;
   }
 
-  weatherTitle.textContent = data.city ? `${data.city}` : '';
-  weatherTitleCity.textContent =
+  weatherTitleElement.textContent = data.city ? `${data.city}` : '';
+  weatherTitleCityElement.textContent =
     data.country && data.region ? `${data.country}, ${data.region}` : '';
-  weatherHumidity.textContent = `Humidity: ${data.humidity}`;
-  weatherCloud.textContent = `Cloud: ${data.cloud} %`;
-  weatherTempC.textContent = `Temperature: ${data.temp_c} C`;
-  weatherVisKm.textContent = `Vis Km: ${data.vis_km} km`;
+  weatherHumidityElement.textContent = `Humidity: ${data.humidity}`;
+  weatherDateElement.textContent = data.localtime ? `Date: ${data.localtime}` : '';
+  weatherCloudElement.textContent = `Cloud: ${data.cloud} %`;
+  weatherTempCElement.textContent = `Temperature: ${data.temp_c} C`;
+  weatherVisKmElement.textContent = `Vis Km: ${data.vis_km} km`;
 
   if (data.imageSrc) {
-    weatherImgBlock.textContent = '';
-    weatherImgBlock.innerHTML = `<img src=${data.imageSrc} alt=${data.imageAlt} />`;
+    weatherImgBlockElement.textContent = '';
+    weatherImgBlockElement.innerHTML = `<img src=${data.imageSrc} alt=${data.imageAlt} />`;
   }
 };
 
-const handleInput = async (e) => {
+const handleSearchInput = async (e) => {
   if (e.key === 'Enter') {
     e.preventDefault();
-    const query = inputSearch.value;
-    console.log(query);
+    const query = inputSearchElement.value;
+    if (!query) return;
 
-    const weatherData = await fetchData(inputValue);
-    renderWeather(weatherData);
-    inputSearch.value = '';
+    try {
+      const weatherData = await fetchData(query);
+      renderWeather(weatherData);
+      inputSearchElement.value = '';
+    } catch (error) {
+      resetWeather();
+      inputSearchElement.value = '';
+    }
   }
 };
 
@@ -77,9 +89,9 @@ const init = async () => {
   try {
     const data = await fetchData();
     renderWeather(data);
-    searchInput.addEventListener('keypress', handleSearchInput);
+    inputSearchElement.addEventListener('keypress', handleSearchInput);
   } catch (error) {
-    console.log(error.message);
+    resetWeather();
   }
 };
 
