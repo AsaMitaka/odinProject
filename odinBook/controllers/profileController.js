dc37ceda672524d78703f0f89fff3720e775dd1b
AsaMitaka <cepelits@gmail.com>
2023-05-21 20:52:10 +0300

    project odinBook profiles

--- odinBook/controllers/profileController.js
+++ odinBook/controllers/profileController.js
@@ -1,7 +1,42 @@
-const profilePageController = (req, res) => {
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const { jwtSecret } = require('../config/config.json');
+
+const profilePageController = async (req, res) => {
   const { id } = req.params;
+  const { token } = req.session;
+  const { userId } = await jwt.verify(token, jwtSecret);
   try {
-    res.send(`Profile ${id}`);
+    const isUser = await User.findById(id);
+    if (!isUser) {
+      res.render('layout', {
+        errorMsg: 'user is not find',
+        contentPage: 'profile',
+        titlePage: 'user profile',
+        profileContent: null,
+        isUser: false,
+      });
+      return;
+    }
+
+    if (id === userId) {
+      res.render('layout', {
+        errorMsg: null,
+        contentPage: 'profile',
+        titlePage: 'user profile',
+        profileContent: isUser,
+        isUser: true,
+      });
+      return;
+    }
+
+    res.render('layout', {
+      errorMsg: null,
+      contentPage: 'profile',
+      titlePage: 'user profile',
+      profileContent: isUser,
+      isUser: false,
+    });
   } catch (error) {
     console.log(error);
   }
